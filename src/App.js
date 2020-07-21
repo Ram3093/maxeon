6645f3a5ca2052e9aec8fa72e4f531450541a164
Ram3093 <65605721+Ram3093@users.noreply.github.com>
2020-07-21 14:42:59 +0530

    configure my app using  components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter  } from 'react-router-dom'
+
+import Dashboard from './components/dashboard/Dashboard'
+
+// import Navbar from './components/navigation/navbar'
 
 
 function App() {
   return (
-    <div className="">
-     <h1>the header</h1>
-    </div>
+    <BrowserRouter>
+              <div className="app">
+                    < Dashboard />
+                    
+                    {/* <Route exact path='/' component={ Navbar } />
+                    <Route path='/products' component={ Navbar } />
+                    <Route path='/features' component={ Navbar } />
+                    <Route path='/usecases' component={ Navbar } />
+                    <Route path='/pricing' component={ Navbar } />
+                    <Route path='/login' component={ Navbar } />  */}
+              </div>
+    </BrowserRouter>
   );
 }
 
